068070906a13c6b621a089aa7801d76d12a7ec90
Tom Stokes <tgstokes@btinternet.com>
2020-07-22 11:21:18 +0100

    start of string finder

--- string_finder.py
+++ string_finder.py
@@ -1,1 +1,12 @@
 # string finder/sorter.py
+file_to_open = "random_text.txt"
+
+def read_file(filename):
+    output_text = []
+    with open(filename) as fo:
+        for line in fo:
+            text = line.strip():
+            output_text.append(text)
+    return output_text
+
+read_file(file_to_open)
